910073440df25836e7c284e9d914960a314601cb
jhw2167 <jhw2167@columbia.edu>
2025-07-15 21:59:52 -0500

    refactor: Add ManagedPlayer import to ServerPlayerMixin

--- common/src/main/java/com/holybuckets/foundation/mixin/ServerPlayerMixin.java
+++ common/src/main/java/com/holybuckets/foundation/mixin/ServerPlayerMixin.java
@@ -1,5 +1,6 @@
 package com.holybuckets.foundation.mixin;
 
+import com.holybuckets.foundation.player.ManagedPlayer;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.Entity;
 import org.spongepowered.asm.mixin.Mixin;

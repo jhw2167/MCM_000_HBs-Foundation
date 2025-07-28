c3c776e295fcd4e50ad43d50eb29085ac613ff59
jhw2167 <jhw2167@columbia.edu>
2025-07-28 00:49:27 -0500

    refactor: Improve code formatting and add concurrent hash map import

--- common/src/main/java/com/holybuckets/foundation/event/EventRegistrar.java
+++ common/src/main/java/com/holybuckets/foundation/event/EventRegistrar.java
@@ -24,6 +24,7 @@ import net.blay09.mods.balm.api.event.server.ServerStoppedEvent;
 import net.minecraft.server.MinecraftServer;
 
 import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.function.Consumer;
 
 
@@ -41,7 +42,7 @@ public class EventRegistrar {
      **/
     private static EventRegistrar instance;
     final Map<Integer, EventPriority> PRIORITIES = new HashMap<>();
-    
+
     final Set<Consumer<PlayerLoginEvent>> ON_PLAYER_LOGIN = new ConcurrentSet<>();
     final Set<Consumer<PlayerLogoutEvent>> ON_PLAYER_LOGOUT = new ConcurrentSet<>();
     final Set<Consumer<LevelLoadingEvent.Load>> ON_LEVEL_LOAD = new ConcurrentSet<>();
@@ -52,13 +53,13 @@ public class EventRegistrar {
     final Set<Consumer<ChunkLoadingEvent.Unload>> ON_CHUNK_UNLOAD = new ConcurrentSet<>();
 
     //final Deque<Consumer<ModLifecycleEvent>> ON_MOD_LIFECYCLE = new ArrayDeque<>();
-        //Will have to divide up into different lifecycles
+    //Will have to divide up into different lifecycles
 
     //final Deque<Consumer<RegisterEvent>> ON_REGISTER = new ArrayDeque<>();
-        //Dont see it, I think this is for registering commands
+    //Dont see it, I think this is for registering commands
 
     //final Deque<Consumer<ModConfigEvent>> ON_MOD_CONFIG = new ArrayDeque<>();
-        //Dont see it, is probably different for forge and fabric, Balm abstracts away all configuration
+    //Dont see it, is probably different for forge and fabric, Balm abstracts away all configuration
 
     final Set<Consumer<ClientStartedEvent>> ON_CLIENT_STARTED_EVENT = new ConcurrentSet<>();
 
@@ -85,10 +86,9 @@ public class EventRegistrar {
     /**
      * Constructor
      **/
-    private EventRegistrar()
-    {
+    private EventRegistrar() {
         super();
-        LoggerBase.logInit( null, "010000", this.getClass().getName());
+        LoggerBase.logInit(null, "010000", this.getClass().getName());
 
         instance = this;
     }
@@ -100,7 +100,6 @@ public class EventRegistrar {
     public static void init() {
         instance = new EventRegistrar();
     }
-
 
 
     //Create public methods for pushing functions onto each function event
@@ -115,35 +114,53 @@ public class EventRegistrar {
         PRIORITIES.put(function.hashCode(), priority);
     }
 
-    public void registerOnPlayerLogin(Consumer<PlayerLoginEvent> function) { registerOnPlayerLogin(function, EventPriority.Normal);}
+    public void registerOnPlayerLogin(Consumer<PlayerLoginEvent> function) {
+        registerOnPlayerLogin(function, EventPriority.Normal);
+    }
+
     public void registerOnPlayerLogin(Consumer<PlayerLoginEvent> function, EventPriority priority) {
         generalRegister(function, ON_PLAYER_LOGIN, priority);
     }
 
-    public void registerOnPlayerLogout(Consumer<PlayerLogoutEvent> function) { registerOnPlayerLogout(function, EventPriority.Normal);}
+    public void registerOnPlayerLogout(Consumer<PlayerLogoutEvent> function) {
+        registerOnPlayerLogout(function, EventPriority.Normal);
+    }
+
     public void registerOnPlayerLogout(Consumer<PlayerLogoutEvent> function, EventPriority priority) {
         generalRegister(function, ON_PLAYER_LOGOUT, priority);
     }
 
 
-    public void registerOnLevelLoad(Consumer<LevelLoadingEvent.Load> function) { registerOnLevelLoad(function, EventPriority.Normal);}
+    public void registerOnLevelLoad(Consumer<LevelLoadingEvent.Load> function) {
+        registerOnLevelLoad(function, EventPriority.Normal);
+    }
+
     public void registerOnLevelLoad(Consumer<LevelLoadingEvent.Load> function, EventPriority priority) {
         generalRegister(function, ON_LEVEL_LOAD, priority);
     }
 
 
-    public void registerOnLevelUnload(Consumer<LevelLoadingEvent.Unload> function) { registerOnLevelUnload(function, EventPriority.Normal);}
+    public void registerOnLevelUnload(Consumer<LevelLoadingEvent.Unload> function) {
+        registerOnLevelUnload(function, EventPriority.Normal);
+    }
+
     public void registerOnLevelUnload(Consumer<LevelLoadingEvent.Unload> function, EventPriority priority) {
         generalRegister(function, ON_LEVEL_UNLOAD, priority);
     }
 
 
-    public void registerOnChunkLoad(Consumer<ChunkLoadingEvent.Load> function) { registerOnChunkLoad(function, EventPriority.Normal);}
+    public void registerOnChunkLoad(Consumer<ChunkLoadingEvent.Load> function) {
+        registerOnChunkLoad(function, EventPriority.Normal);
+    }
+
     public void registerOnChunkLoad(Consumer<ChunkLoadingEvent.Load> function, EventPriority priority) {
         generalRegister(function, ON_CHUNK_LOAD, priority);
     }
 
-    public void registerOnChunkUnload(Consumer<ChunkLoadingEvent.Unload> function) { registerOnChunkUnload(function, EventPriority.Normal);}
+    public void registerOnChunkUnload(Consumer<ChunkLoadingEvent.Unload> function) {
+        registerOnChunkUnload(function, EventPriority.Normal);
+    }
+
     public void registerOnChunkUnload(Consumer<ChunkLoadingEvent.Unload> function, EventPriority priority) {
         generalRegister(function, ON_CHUNK_UNLOAD, priority);
     }
@@ -165,39 +182,60 @@ public class EventRegistrar {
     }
     */
 
-    public void registerOnClientStarted(Consumer<ClientStartedEvent> function) { registerOnClientStarted(function, EventPriority.Normal);}
+    public void registerOnClientStarted(Consumer<ClientStartedEvent> function) {
+        registerOnClientStarted(function, EventPriority.Normal);
+    }
+
     public void registerOnClientStarted(Consumer<ClientStartedEvent> function, EventPriority priority) {
         generalRegister(function, ON_CLIENT_STARTED_EVENT, priority);
     }
 
-    public void registerOnBeforeServerStarted(Consumer<ServerStartingEvent> function) { registerOnBeforeServerStarted(function, EventPriority.Normal);}
+    public void registerOnBeforeServerStarted(Consumer<ServerStartingEvent> function) {
+        registerOnBeforeServerStarted(function, EventPriority.Normal);
+    }
+
     public void registerOnBeforeServerStarted(Consumer<ServerStartingEvent> function, EventPriority priority) {
         generalRegister(function, ON_BEFORE_SERVER_START, priority);
     }
 
 
-    public void registerOnServerStarted(Consumer<ServerStartedEvent> function) { registerOnServerStarted(function, EventPriority.Normal);}
+    public void registerOnServerStarted(Consumer<ServerStartedEvent> function) {
+        registerOnServerStarted(function, EventPriority.Normal);
+    }
+
     public void registerOnServerStarted(Consumer<ServerStartedEvent> function, EventPriority priority) {
         generalRegister(function, ON_SERVER_START, priority);
     }
 
-    public void registerOnServerStopped(Consumer<ServerStoppedEvent> function) { registerOnServerStopped(function, EventPriority.Normal);}
+    public void registerOnServerStopped(Consumer<ServerStoppedEvent> function) {
+        registerOnServerStopped(function, EventPriority.Normal);
+    }
+
     public void registerOnServerStopped(Consumer<ServerStoppedEvent> function, EventPriority priority) {
         generalRegister(function, ON_SERVER_STOP, priority);
     }
 
-    public void registerOnConnectedToServer(Consumer<ConnectedToServerEvent> function) { registerOnConnectedToServer(function, EventPriority.Normal);}
+    public void registerOnConnectedToServer(Consumer<ConnectedToServerEvent> function) {
+        registerOnConnectedToServer(function, EventPriority.Normal);
+    }
+
     public void registerOnConnectedToServer(Consumer<ConnectedToServerEvent> function, EventPriority priority) {
         generalRegister(function, ON_CONNECTED_TO_SERVER, priority);
     }
 
-    public void registerOnDisconnectedFromServer(Consumer<DisconnectedFromServerEvent> function) { registerOnDisconnectedFromServer(function, EventPriority.Normal);}
+    public void registerOnDisconnectedFromServer(Consumer<DisconnectedFromServerEvent> function) {
+        registerOnDisconnectedFromServer(function, EventPriority.Normal);
+    }
+
     public void registerOnDisconnectedFromServer(Consumer<DisconnectedFromServerEvent> function, EventPriority priority) {
         generalRegister(function, ON_DISCONNECTED_FROM_SERVER, priority);
     }
 
 
-    public void registerOnDataSave(Consumer<DatastoreSaveEvent> function) { registerOnDataSave(function, EventPriority.Normal);}
+    public void registerOnDataSave(Consumer<DatastoreSaveEvent> function) {
+        registerOnDataSave(function, EventPriority.Normal);
+    }
+
     public void registerOnDataSave(Consumer<DatastoreSaveEvent> function, EventPriority priority) {
         generalRegister(function, ON_DATA_SAVE, priority);
     }
@@ -214,6 +252,7 @@ public class EventRegistrar {
     public <T extends ServerTickEvent> void registerOnServerTick(TickType type, Consumer<T> function, EventPriority priority) {
         generalTickEventRegister(function, SERVER_TICK_EVENTS, type, priority);
     }
+
 
     @SuppressWarnings("unchecked")
     public <T extends ClientTickEvent> void registerOnClientTick(TickType type, Consumer<T> function) {
@@ -236,57 +275,78 @@ public class EventRegistrar {
     }
 
 
-
-
     //** PLAYER EVENTS
 
-    public void registerOnPlayerAttack(Consumer<PlayerAttackEvent> function) { registerOnPlayerAttack(function, EventPriority.Normal); }
+    public void registerOnPlayerAttack(Consumer<PlayerAttackEvent> function) {
+        registerOnPlayerAttack(function, EventPriority.Normal);
+    }
+
     public void registerOnPlayerAttack(Consumer<PlayerAttackEvent> function, EventPriority priority) {
         generalRegister(function, ON_PLAYER_ATTACK, priority);
     }
 
-    public void registerOnBreakBlock(Consumer<BreakBlockEvent> function) { registerOnBreakBlock(function, EventPriority.Normal); }
+    public void registerOnBreakBlock(Consumer<BreakBlockEvent> function) {
+        registerOnBreakBlock(function, EventPriority.Normal);
+    }
+
     public void registerOnBreakBlock(Consumer<BreakBlockEvent> function, EventPriority priority) {
         generalRegister(function, ON_BLOCK_BROKEN, priority);
     }
 
-    public void registerOnPlayerChangedDimension(Consumer<PlayerChangedDimensionEvent> function) { registerOnPlayerChangedDimension(function, EventPriority.Normal); }
+    public void registerOnPlayerChangedDimension(Consumer<PlayerChangedDimensionEvent> function) {
+        registerOnPlayerChangedDimension(function, EventPriority.Normal);
+    }
+
     public void registerOnPlayerChangedDimension(Consumer<PlayerChangedDimensionEvent> function, EventPriority priority) {
         generalRegister(function, ON_PLAYER_CHANGED_DIMENSION, priority);
     }
 
-    public void registerOnPlayerRespawn(Consumer<PlayerRespawnEvent> function) { registerOnPlayerRespawn(function, EventPriority.Normal); }
+    public void registerOnPlayerRespawn(Consumer<PlayerRespawnEvent> function) {
+        registerOnPlayerRespawn(function, EventPriority.Normal);
+    }
+
     public void registerOnPlayerRespawn(Consumer<PlayerRespawnEvent> function, EventPriority priority) {
         generalRegister(function, ON_PLAYER_RESPAWN, priority);
     }
 
-    public void registerOnPlayerDeath(Consumer<LivingDeathEvent> function) { registerOnPlayerDeath(function, EventPriority.Normal); }
+    public void registerOnPlayerDeath(Consumer<LivingDeathEvent> function) {
+        registerOnPlayerDeath(function, EventPriority.Normal);
+    }
+
     public void registerOnPlayerDeath(Consumer<LivingDeathEvent> function, EventPriority priority) {
         generalRegister(function, ON_PLAYER_DEATH, priority);
     }
 
-    public void registerOnUseBlock(Consumer<UseBlockEvent> function) { registerOnUseBlock(function, EventPriority.Normal); }
+    public void registerOnUseBlock(Consumer<UseBlockEvent> function) {
+        registerOnUseBlock(function, EventPriority.Normal);
+    }
+
     public void registerOnUseBlock(Consumer<UseBlockEvent> function, EventPriority priority) {
         generalRegister(function, ON_USE_BLOCK, priority);
     }
 
-    public void registerOnPlayerAttackEvent(Consumer<PlayerAttackEvent> function) { registerOnPlayerAttackEvent(function, EventPriority.Normal); }
+    public void registerOnPlayerAttackEvent(Consumer<PlayerAttackEvent> function) {
+        registerOnPlayerAttackEvent(function, EventPriority.Normal);
+    }
+
     public void registerOnPlayerAttackEvent(Consumer<PlayerAttackEvent> function, EventPriority priority) {
         generalRegister(function, ON_PLAYER_ATTACK_EVENT, priority);
     }
 
-    public void registerOnDigSpeedEvent(Consumer<DigSpeedEvent> function) { registerOnDigSpeedEvent(function, EventPriority.Normal); }
+    public void registerOnDigSpeedEvent(Consumer<DigSpeedEvent> function) {
+        registerOnDigSpeedEvent(function, EventPriority.Normal);
+    }
+
     public void registerOnDigSpeedEvent(Consumer<DigSpeedEvent> function, EventPriority priority) {
         generalRegister(function, ON_DIG_SPEED_EVENT, priority);
     }
 
 
+    /**
+     * Custom Events
+     **/
 
-
-    /** Custom Events **/
-
-    public void dataSaveEvent()
-    {
+    public void dataSaveEvent() {
         DatastoreSaveEvent event = DatastoreSaveEvent.create();
         for (Consumer<DatastoreSaveEvent> saver : ON_DATA_SAVE) {
             saver.accept(event);
@@ -297,7 +357,7 @@ public class EventRegistrar {
     public void onServerTick(MinecraftServer server) {
         long totalTicks = GeneralConfig.getInstance().getTotalTickCount();
         ServerTickEvent event = new ServerTickEvent(totalTicks);
-        
+
         SERVER_TICK_EVENTS.forEach((scheme, consumer) -> {
             if (totalTicks % scheme.getFrequency() == scheme.offset) {
                 consumer.accept(event);
@@ -305,7 +365,9 @@ public class EventRegistrar {
         });
     }
 
-    /** ############### **/
+    /**
+     * ###############
+     **/
 
     private class TickScheme {
         int offset;
@@ -332,7 +394,8 @@ public class EventRegistrar {
                     return 1;
             }
 
-    }
+        }
 
+    }
 }
 //END CLASS

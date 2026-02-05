7b81aa88e3be82ce5ccca6ac60fd0dc79a8c9b00
jhw2167 <jhw2167@columbia.edu>
2026-02-05 00:00:15 -0600

    feat: add EssenceDataJsonConfig for managing essence configurations

---
+++ common/src/main/java/com/holybuckets/foundation/config/model/EssenceDataJsonConfig.java
@@ -0,0 +1,248 @@
+package com.holybuckets.foundation.config.model;
+
+import com.google.gson.*;
+import com.holybuckets.foundation.modelInterface.IStringSerializable;
+import net.minecraft.resources.ResourceLocation;
+
+import java.util.*;
+
+public class EssenceDataJsonConfig implements IStringSerializable {
+
+    public static final String DEF_ESSENCE_FILE_CONFIG_PATH = "config/HBFoundationEssenceConfig.json";
+    public static final String DEFAULT_CONFIG = buildDefaultConfig();
+
+    private JsonObject rootObject;
+    private Map<String, EssenceConfig> essenceMap;
+
+    public class EssenceConfig {
+        public String id;
+        public Set<String> biomes;
+        public Set<String> entities;
+        public Set<String> dimensions;
+        public Set<String> levels;
+        public Set<String> items;
+
+        public EssenceConfig(String id, Set<String> biomes, Set<String> entities, Set<String> dimensions, Set<String> levels, Set<String> items) {
+            this.id = id;
+            this.biomes = biomes;
+            this.entities = entities;
+            this.dimensions = dimensions;
+            this.levels = levels;
+            this.items = items;
+        }
+
+        public EssenceConfig(String id, JsonObject obj) {
+            this.id = id;
+            this.biomes = parseResourceLocationArray(obj, "biomes");
+            this.entities = parseResourceLocationArray(obj, "entities");
+            this.dimensions = parseResourceLocationArray(obj, "dimensions");
+            this.levels = parseResourceLocationArray(obj, "levels");
+            this.items = parseResourceLocationArray(obj, "items");
+        }
+    }
+
+    public EssenceDataJsonConfig(String jsonString) {
+        deserialize(jsonString);
+    }
+
+    @Override
+    public String serialize() {
+        if (rootObject == null) {
+            return "{}";
+        }
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        return gson.toJson(rootObject);
+    }
+
+    @Override
+    public void deserialize(String jsonString) {
+        Gson gson = new Gson();
+        rootObject = gson.fromJson(jsonString, JsonObject.class);
+        essenceMap = new HashMap<>();
+
+        if (rootObject != null && rootObject.has("essenceConfigs")) {
+            JsonArray essenceArray = rootObject.getAsJsonArray("essenceConfigs");
+
+            for (JsonElement element : essenceArray) {
+                if (element.isJsonObject()) {
+                    JsonObject essenceObj = element.getAsJsonObject();
+                    String id = essenceObj.get("id").getAsString();
+                    essenceMap.put(id, new EssenceConfig(id, essenceObj));
+                }
+            }
+        }
+    }
+
+    private Set<String> parseResourceLocationArray(JsonObject obj, String fieldName) {
+        Set<String> set = new HashSet<>();
+        if (obj.has(fieldName)) {
+            JsonArray array = obj.getAsJsonArray(fieldName);
+            for (JsonElement element : array) {
+                if (element.isJsonPrimitive())
+                    set.add(element.getAsString());
+            }
+        }
+
+        return set.isEmpty() ? null : set;
+    }
+
+    public Set<String> getAllEssenceIds() {
+        return essenceMap.keySet();
+    }
+
+    private static String buildDefaultConfig() {
+        List<EssenceConfig> configs = new ArrayList<>();
+        EssenceDataJsonConfig temp = new EssenceDataJsonConfig("{}");
+
+        // Plains Essence
+        configs.add(temp.new EssenceConfig("plains",
+            Set.of("minecraft:plains", "minecraft:sunflower_plains", "minecraft:meadow", "minecraft:savanna", "minecraft:savanna_plateau", "minecraft:windswept_savanna"),
+            Set.of("minecraft:cow", "minecraft:sheep", "minecraft:pig", "minecraft:chicken", "minecraft:horse", "minecraft:donkey"),
+            null, null, null));
+
+        // Jungle Essence
+        configs.add(temp.new EssenceConfig("jungle",
+            Set.of("minecraft:jungle", "minecraft:sparse_jungle", "minecraft:bamboo_jungle"),
+            Set.of("minecraft:parrot", "minecraft:ocelot", "minecraft:panda"),
+            null, null, null));
+
+        // Swampy Essence
+        configs.add(temp.new EssenceConfig("swampy",
+            Set.of("minecraft:swamp", "minecraft:mangrove_swamp"),
+            Set.of("minecraft:slime", "minecraft:frog", "minecraft:witch"),
+            null, null, null));
+
+        // Cold Essence
+        configs.add(temp.new EssenceConfig("cold",
+            Set.of("minecraft:snowy_plains", "minecraft:ice_spikes", "minecraft:snowy_taiga", "minecraft:snowy_beach", "minecraft:frozen_river", "minecraft:frozen_peaks", "minecraft:snowy_slopes"),
+            Set.of("minecraft:stray", "minecraft:polar_bear", "minecraft:snow_golem"),
+            null, null, null));
+
+        // Snowy Essence
+        configs.add(temp.new EssenceConfig("snowy",
+            Set.of("minecraft:snowy_plains", "minecraft:ice_spikes", "minecraft:snowy_taiga", "minecraft:snowy_beach", "minecraft:frozen_river", "minecraft:grove"),
+            Set.of("minecraft:polar_bear", "minecraft:snow_golem", "minecraft:fox", "minecraft:rabbit"),
+            null, null, null));
+
+        // Ocean Essence
+        configs.add(temp.new EssenceConfig("ocean",
+            Set.of("minecraft:ocean", "minecraft:deep_ocean", "minecraft:cold_ocean", "minecraft:lukewarm_ocean", "minecraft:warm_ocean", "minecraft:frozen_ocean", "minecraft:deep_cold_ocean", "minecraft:deep_lukewarm_ocean", "minecraft:deep_frozen_ocean"),
+            Set.of("minecraft:squid", "minecraft:dolphin", "minecraft:cod", "minecraft:salmon", "minecraft:tropical_fish", "minecraft:pufferfish", "minecraft:drowned", "minecraft:guardian"),
+            null, null, null));
+
+        // Peaks Essence
+        configs.add(temp.new EssenceConfig("peaks",
+            Set.of("minecraft:jagged_peaks", "minecraft:frozen_peaks", "minecraft:stony_peaks", "minecraft:windswept_hills", "minecraft:windswept_gravelly_hills", "minecraft:windswept_forest"),
+            Set.of("minecraft:goat", "minecraft:llama"),
+            null, null, null));
+
+        // Deep Essence
+        configs.add(temp.new EssenceConfig("deep",
+            Set.of("minecraft:deep_dark", "minecraft:deep_ocean", "minecraft:deep_cold_ocean", "minecraft:deep_lukewarm_ocean", "minecraft:deep_frozen_ocean"),
+            Set.of("minecraft:warden", "minecraft:elder_guardian", "minecraft:guardian"),
+            null, null, null));
+
+        // Sunny Essence
+        configs.add(temp.new EssenceConfig("sunny",
+            Set.of("minecraft:desert", "minecraft:badlands", "minecraft:eroded_badlands", "minecraft:wooded_badlands", "minecraft:savanna", "minecraft:sunflower_plains"),
+            Set.of("minecraft:rabbit", "minecraft:husk"),
+            null, null, null));
+
+        // Maple Essence
+        configs.add(temp.new EssenceConfig("maple",
+            Set.of("minecraft:forest", "minecraft:birch_forest", "minecraft:old_growth_birch_forest", "minecraft:flower_forest"),
+            Set.of("minecraft:bee", "minecraft:wolf"),
+            null, null, null));
+
+        // Cherry Essence
+        configs.add(temp.new EssenceConfig("cherry",
+            Set.of("minecraft:cherry_grove"),
+            Set.of("minecraft:bee", "minecraft:pig", "minecraft:sheep"),
+            null, null, null));
+
+        // Mushroom Essence
+        configs.add(temp.new EssenceConfig("mushroom",
+            Set.of("minecraft:mushroom_fields"),
+            Set.of("minecraft:mooshroom"),
+            null, null, null));
+
+        // Witching Essence
+        configs.add(temp.new EssenceConfig("witching",
+            Set.of("minecraft:swamp", "minecraft:dark_forest"),
+            Set.of("minecraft:witch", "minecraft:bat", "minecraft:spider", "minecraft:cave_spider"),
+            null, null, null));
+
+        // Fire Essence
+        configs.add(temp.new EssenceConfig("fire",
+            Set.of("minecraft:nether_wastes", "minecraft:crimson_forest", "minecraft:warped_forest", "minecraft:soul_sand_valley", "minecraft:basalt_deltas"),
+            Set.of("minecraft:blaze", "minecraft:magma_cube", "minecraft:ghast"),
+            Set.of("minecraft:the_nether"), null, null));
+
+        // Hot Essence
+        configs.add(temp.new EssenceConfig("hot",
+            Set.of("minecraft:desert", "minecraft:badlands", "minecraft:eroded_badlands", "minecraft:savanna", "minecraft:nether_wastes", "minecraft:basalt_deltas"),
+            Set.of("minecraft:blaze", "minecraft:strider", "minecraft:husk"),
+            null, null, null));
+
+        // Hellish Essence
+        configs.add(temp.new EssenceConfig("hellish",
+            Set.of("minecraft:nether_wastes", "minecraft:crimson_forest", "minecraft:warped_forest", "minecraft:soul_sand_valley", "minecraft:basalt_deltas"),
+            Set.of("minecraft:blaze", "minecraft:ghast", "minecraft:magma_cube", "minecraft:zombified_piglin", "minecraft:piglin", "minecraft:piglin_brute", "minecraft:hoglin", "minecraft:wither_skeleton"),
+            Set.of("minecraft:the_nether"), null, null));
+
+        // Void Essence
+        configs.add(temp.new EssenceConfig("void",
+            Set.of("minecraft:the_end", "minecraft:end_highlands", "minecraft:end_midlands", "minecraft:end_barrens", "minecraft:small_end_islands"),
+            Set.of("minecraft:enderman", "minecraft:endermite", "minecraft:shulker", "minecraft:ender_dragon"),
+            Set.of("minecraft:the_end"), null, null));
+
+        return serializeConfigs(configs);
+    }
+
+    private static String serializeConfigs(List<EssenceConfig> configs) {
+        JsonObject root = new JsonObject();
+        JsonArray essenceArray = new JsonArray();
+
+        for (EssenceConfig config : configs) {
+            JsonObject essenceObj = new JsonObject();
+            essenceObj.addProperty("id", config.id);
+
+            if (config.biomes != null) {
+                JsonArray biomesArray = new JsonArray();
+                config.biomes.forEach(biomesArray::add);
+                essenceObj.add("biomes", biomesArray);
+            }
+
+            if (config.entities != null) {
+                JsonArray entitiesArray = new JsonArray();
+                config.entities.forEach(entitiesArray::add);
+                essenceObj.add("entities", entitiesArray);
+            }
+
+            if (config.dimensions != null) {
+                JsonArray dimensionsArray = new JsonArray();
+                config.dimensions.forEach(dimensionsArray::add);
+                essenceObj.add("dimensions", dimensionsArray);
+            }
+
+            if (config.levels != null) {
+                JsonArray levelsArray = new JsonArray();
+                config.levels.forEach(levelsArray::add);
+                essenceObj.add("levels", levelsArray);
+            }
+
+            if (config.items != null) {
+                JsonArray itemsArray = new JsonArray();
+                config.items.forEach(itemsArray::add);
+                essenceObj.add("items", itemsArray);
+            }
+
+            essenceArray.add(essenceObj);
+        }
+
+        root.add("essenceConfigs", essenceArray);
+
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        return gson.toJson(root);
+    }
+}

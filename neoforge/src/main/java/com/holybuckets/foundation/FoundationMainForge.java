2cd145e03d59730b8ed431db802a567a329c1e55
jhw2167 <jhw2167@columbia.edu>
2026-05-05 01:20:39 -0500

    Reworked neoforge entry point files - removed old forge mods.toml and services files - removed forge project

--- neoforge/src/main/java/com/holybuckets/foundation/FoundationMainForge.java
+++ neoforge/src/main/java/com/holybuckets/foundation/FoundationMainForge.java
@@ -4,9 +4,8 @@ import com.holybuckets.foundation.capability.FoundationAttachments;
 import com.holybuckets.foundation.client.CommonClassClient;
 import net.blay09.mods.balm.api.Balm;
 import net.blay09.mods.balm.api.client.BalmClient;
-import net.neoforged.api.distmarker.Dist;
+import net.blay09.mods.balm.neoforge.NeoForgeLoadContext;
 import net.neoforged.bus.api.IEventBus;
-import net.neoforged.fml.DistExecutor;
 import net.neoforged.fml.common.Mod;
 
 @Mod(Constants.MOD_ID)
@@ -15,12 +14,9 @@ public class FoundationMainForge {
     public FoundationMainForge(IEventBus modEventBus) {
         super();
         // Register NeoForge attachment types to the mod event bus
+        final var context = new NeoForgeLoadContext(modEventBus);
         FoundationAttachments.ATTACHMENT_TYPES.register(modEventBus);
-
-        Balm.initialize(Constants.MOD_ID, CommonClass::init);
-        DistExecutor.runWhenOn(Dist.CLIENT, () -> () -> BalmClient.initialize(Constants.MOD_ID, CommonClassClient::initClient));
-
-        FoundationAttachments.init();
+        Balm.initialize(Constants.MOD_ID, context, CommonClass::init);
     }
 
 }
